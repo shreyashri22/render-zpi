c3e4989a65b6709581ed4506ca47b4a9df6f899d
shreyashri22 <144883263+shreyashri22@users.noreply.github.com>
2023-09-26 02:21:01 +0530

    Update api-endpoint.py

--- api-endpoint.py
+++ api-endpoint.py
@@ -1,25 +1,22 @@
+from flask import Flask, jsonify,request
+# from flask_restful import Resource,Api
 from maincode import Ask_bot
-from flask import Flask, jsonify
-from flask_restful import Resource,Api
 
 # load_dotenv()
 app=Flask("askbot")
-api=Api(app)
+# api=Api(app)
 
-class Hi(Resource):
-    def get(self):
-        return("Hello")
+@app.route('/',methods=['GET','POST'])
+def Hi():
+    if (request.method=='GET'):
+        data='hello'
+        return jsonify({'data':data})
 
-class Ask(Resource):
-    def get(self, query,session_id):
+@app.route('/ask/<string:session_id>&<string:query>',methods = ['GET'])
+def Ask(session_id,query):
+    return Ask_bot(query,session_id)
   
-        return jsonify({"Response":Ask_bot(query,session_id)})
-  
-  
-# adding the defined resources along with their corresponding urls
-api.add_resource(Ask, '/ask/<session_id>/<query>')
-api.add_resource(Hi,"/")
 
 if __name__ == '__main__':
   
-    app.run(debug = True)
+    app.run(debug = True,host="0.0.0.0")
